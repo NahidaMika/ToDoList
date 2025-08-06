ba491677857b71ea344c9c17080d88aa9b83a596
Nahida <51387274+NahidaMika@users.noreply.github.com>
2025-08-06 15:04:58 -0600

    Added meowfacts api

--- scripts/HTMLRequestsHandler.py
+++ scripts/HTMLRequestsHandler.py
@@ -3,6 +3,7 @@ import os
 import requests
 from pathlib import Path, PureWindowsPath
 import webbrowser
+from time import sleep
 
 #File paths
 json_file_path_windows = PureWindowsPath('data\\json\\a')
@@ -17,7 +18,7 @@ api_file_path_windows = PureWindowsPath('data\\api-keys\\a')
 api_file_path_not_final = str(Path(api_file_path_windows))
 api_file_path = api_file_path_not_final[:-1]
 
-def check_for_necesary_paths():
+def check_for_necesary_paths(): #Check if the path for files exist
     if not os.path.exists(json_file_path):
         os.makedirs(json_file_path)
         print(f'Path "{json_file_path}" created')
@@ -30,10 +31,10 @@ def check_for_necesary_paths():
     else:
         print(f'Path "{api_file_path}" already exists')
 
-def check_for_necesary_files():
+def check_for_necesary_files(): #Check if the files exist
     if not os.path.exists(json_file_path + 'ToDoList.json'):
         with open(json_file_path + 'ToDoList.json', 'xt') as json_file:
-            json_file.write({"date":"","todo":[{"task":"","status":"Not completed"}]})
+            json.dump({"date":"","todo":[{"task":"","status":"Not completed"}]}, json_file)
         print('File "ToDoList.json" created')
     else:
         print('File "ToDoList.json" already exists')
@@ -79,13 +80,13 @@ get_file_id_from_file = open(api_file_path + 'FileID', 'r')
 get_file_id_from_file = get_file_id_from_file.read()
 FileId = get_file_id_from_file.strip()
 
-def jsonbin_io_main():
+def jsonbin_io_main(): # Opens jsonbin.io
     webbrowser.open('https://jsonbin.io/')
 
-def jsonbin_io_bins():
+def jsonbin_io_bins(): # Opens jsonbin.io Bins
     webbrowser.open('https://jsonbin.io/app/bins')
 
-def check_json():
+def check_json(): # Checks if there is a json file and renames it
     try:
         os.rename(json_file_path + f'ToDoList.json', json_file_path + f'ToDoList.json.old')
     except FileExistsError:
@@ -94,7 +95,7 @@ def check_json():
     except FileNotFoundError:
         pass
 
-def json_backup_checker(): # Checks if there is a json file and renames it and move it to the backup folder
+def json_backup_checker(): # Checks if there is a json file and renames it 
     try:
         os.rename(json_file_path + f'ToDoList.json', json_file_path + f'ToDoList.json.backup')
     except FileExistsError:
@@ -103,12 +104,12 @@ def json_backup_checker(): # Checks if there is a json file and renames it and m
     except FileNotFoundError:
         pass
 
-def save_backup_jsonbin(data):
+def save_backup_jsonbin(data): #Saves the json file backup
     json_backup_checker()
     with open(json_file_path + 'ToDoList.json', 'xt') as json_file:
         json.dump(data, json_file)
 
-def upload_jsonbin(jsondata): #May never be in use
+def upload_jsonbin(jsondata): # Uploads the json file
     url = f'https://api.jsonbin.io/v3/b/{FileId}'
     headers = {
     'Content-Type': 'application/json',
@@ -123,7 +124,19 @@ def upload_jsonbin(jsondata): #May never be in use
         print(req.status_code, req.reason)
         print('Failed to upload file')
 
-def download_jsonbin():
+def load_jsonbin(): # Loads the json file contents
+    try:
+        json_path = open(json_file_path + 'ToDoList.json', 'r')
+        json_data = json.load(json_path)
+        json_path.close()
+
+        return json_data
+    
+    except FileNotFoundError:
+        print('File not found\n' \
+        'Please download the file')
+
+def download_jsonbin(): # Downloads the json file contents
     url = f'https://api.jsonbin.io/v3/b/{FileId}'
     headers = {
     'X-Master-Key': key.strip(),
@@ -142,19 +155,9 @@ def download_jsonbin():
         print(req.status_code, req.reason)
         print('Failed to download file')
 
-def load_jsonbin(): #May never be in use
-    try:
-        json_path = open(json_file_path + 'ToDoList.json', 'r')
-        json_data = json.load(json_path)
-        json_path.close()
 
-        return json_data
-    
-    except FileNotFoundError:
-        print('File not found\n' \
-        'Please download the file')
 
-def make_jsonbin(jsondata, apikey):
+def make_jsonbin(jsondata, apikey): # Uploads a new json file
     url = 'https://api.jsonbin.io/v3/b'
     headers = {
     'Content-Type': 'application/json',
@@ -171,7 +174,33 @@ def make_jsonbin(jsondata, apikey):
 
     return req
 
+def meow_facts(count: int = 1) -> dict: # Gets meow facts
+    meow_facts_dict = {}
+    url = f'https://meowfacts.herokuapp.com/?count={count}' #&lang={lang}
+
+    req = requests.request("GET", url)
+
+    if req.status_code == 200:
+            meow_facts = req.text
+            meow_facts = json.loads(meow_facts)
+            meow_facts_dict = meow_facts
+
+            return meow_facts_dict
+
+    else:
+        print(req.status_code, req.reason)
+        print('Failed to get facts')
+
+    return meow_facts_dict
+
+def get_meow_facts(timeout: int = 300, count: int = 0): # Gets meow facts
+    while True:
+        fact = meow_facts()
+        print(fact['data'][count])
+        sleep(timeout)
+
 if __name__ == '__main__':
     #download_jsonbin()
     #check_for_necesary_files()
+    #meow_facts(100)
     pass
